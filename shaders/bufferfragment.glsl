698f2a054ca870c2f76101aa19c58d99b4fa7ca9
Ambrose <37660542+Turpan@users.noreply.github.com>
2018-12-12 20:25:39 +1000

    HDR
    Added HDR colour filtering to the final pass shader.

--- shaders/bufferfragment.glsl
+++ shaders/bufferfragment.glsl
@@ -4,9 +4,20 @@ uniform sampler2D render;
 
 uniform float gamma;
 
+uniform float exposure;
+
 in vec2 texcoord;
 
 out vec4 outColour;
+
+vec4 hdrCorrection(vec4 fragColour) {
+	vec3 hdr = fragColour.rgb;
+	
+	//vec3 ldr = hdr / (hdr + vec3(1.0));
+	vec3 ldr = vec3(1.0) - exp(-hdr * exposure);
+	
+	return vec4(ldr, 1.0);
+}
 
 vec4 gammaCorrection(vec4 fragColour) {
 	fragColour.rgb = pow(fragColour.rgb, vec3(1.0/gamma));
@@ -15,5 +26,6 @@ vec4 gammaCorrection(vec4 fragColour) {
 
 void main(void) {
 	outColour = vec4(texture(render, texcoord).rgb, 1.0);
+	outColour = hdrCorrection(outColour);
 	outColour = gammaCorrection(outColour);
 }
